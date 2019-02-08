74703a78bc27ec07b1f6751887397ec600c15bf6
MikeElMagnifico <miguelmmendeza@gmail.com>
2019-02-08 09:17:40 -0600

    Quiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,25 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {HttpClientModule} from '@angular/common/http'
+import { CasaPage } from '../pages/casa/casa';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CasaPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CasaPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { CasaPage } from '../casa/casa';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  casas = [];
+  casaPage = CasaPage;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public http: HttpClient) {
 
+      this.http.get('/v1/api/pin-data?url=/s-renta-inmuebles/guadalajara-y-zona-metro/v1c1098l10567p1&geo=(21.10631012145462,-102.42214381725364),(20.21712862656199,-104.32387728274637)')
+      .subscribe(data => {
+        //console.log(JSON.stringify(data));
+        if (data.hasOwnProperty('ads')){
+          this.casas = data['ads'];
+        }
+      },
+        error=> {
+          console.log(JSON.stringify(error));
+        }
+      )
+  }
+
+  verCasa(casa){
+    this.navCtrl.push(this.casaPage, {casa: casa});
   }
 
 }
